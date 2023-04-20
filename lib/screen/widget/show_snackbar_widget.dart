4355e096ef40cf312a2c054948ded5c8246e1dfa
BigAchiever <danishali9575@gmail.com>
2023-04-21 00:07:20 +0530

    fixes

--- lib/screen/widget/show_snackbar_widget.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showSnackbar(BuildContext context, String content, {int duration = 5}) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    duration: Duration(seconds: duration),
-    content: Text(content),
-  ));
-}
